78b55e51427c51eb6ce1a8ce8bb7563c2e821232
Neson <neson@dex.tw>
2014-05-08 22:35:58 +0800

    update

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -20,7 +20,20 @@ if(Modernizr.touch) {
 // AppID
 PatwFB.appId = '166332873401574';
 // Init
-PatwFB.init();
+PatwFB.init(function () {
+	PatwFB.setSize();
+
+	FB.getLoginStatus(function(response) {
+		if (response.status === 'connected') {
+			Facebook_Login();
+		} else if (response.status === 'not_authorized') {
+			// the user is logged in to Facebook,
+			// but has not authenticated your app
+		} else {
+			// the user isn't logged in to Facebook.
+		}
+	});
+});
 // scope. reference: https://developers.facebook.com/docs/reference/api/permissions/
 PatwFB.scope = "user_likes,email";
 
@@ -256,21 +269,6 @@ function refreshViev() {
 }
 
 $(function() {
-
-	PatwFB.init(function () {
-		PatwFB.setSize();
-
-		FB.getLoginStatus(function(response) {
-			if (response.status === 'connected') {
-				Facebook_Login();
-			} else if (response.status === 'not_authorized') {
-				// the user is logged in to Facebook,
-				// but has not authenticated your app
-			} else {
-				// the user isn't logged in to Facebook.
-			}
-		});
-	});
 
 	$('a[href*=#]:not([href=#])').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
